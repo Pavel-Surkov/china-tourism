2aa29082008da943b3e3934f6d666ee469a594bc
Pavel Surkov <surkovv.pavel@gmail.com>
2023-05-13 17:42:24 +0700

    adds adaptive hero section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const heroSwiper = new Swiper('.hero-swiper', {
+  loop: true,
+  allowTouchMove: false,
+  navigation: {
+    prevEl: '.hero-btn-prev',
+    nextEl: '.hero-btn-next',
+  },
+});
+
+(function handleMenu() {
+  const menu = document.querySelector('#menu');
+  const menuBtn = document.querySelector('#menu-btn');
+  const menuClose = document.querySelector('#menu-close-btn');
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.add('menu_opened');
+    document.documentElement.classList.add('is-locked');
+  });
+
+  menuClose.addEventListener('click', () => {
+    menu.classList.remove('menu_opened');
+    document.documentElement.classList.remove('is-locked');
+  });
+})();
