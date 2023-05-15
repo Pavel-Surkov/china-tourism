730ef20ff11383e4945cb3930a0a772dd4767849
Pavel Surkov <surkovv.pavel@gmail.com>
2023-05-16 00:14:56 +0700

    adds js for program  modals

--- js/main.js
+++ js/main.js
@@ -27,4 +27,60 @@ const heroSwiper = new Swiper('.hero-swiper', {
 
 Fancybox.bind('[data-fancybox="gallery"]', {});
 
-(function handleProgramModals() {})();
+function openModal(modal, blocker) {
+  modal.classList.add('modal_opened');
+  document.documentElement.classList.add('is-locked');
+  blocker.classList.add('blocker_opened');
+}
+
+function closeModal(modal, blocker) {
+  modal.classList.remove('modal_opened');
+  document.documentElement.classList.remove('is-locked');
+  blocker.classList.remove('blocker_opened');
+}
+
+(function handleProgramModals() {
+  const programItems = document.querySelectorAll('.program-item');
+  const programModals = Array.from(document.querySelectorAll('.program-modal'));
+  const programWrapper = document.querySelector('.program-wrapper');
+
+  if (programItems.length && programModals.length && programWrapper) {
+    programWrapper.addEventListener('click', (evt) => {
+      const target = evt.target;
+      const btn = target.closest('.program-item__btn');
+
+      if (btn) {
+        const modalId = btn.dataset.modal;
+        const modal = programModals.find((modal) => modal.id === modalId);
+
+        if (modal) {
+          const blocker = document.querySelector('#blocker');
+          openModal(modal, blocker);
+        }
+      }
+    });
+  }
+})();
+
+(function handleProgramModalsClose() {
+  const programModals = Array.from(document.querySelectorAll('.program-modal'));
+
+  if (programModals.length) {
+    const blocker = document.querySelector('#blocker');
+
+    const closeBtns = programModals.map((modal) =>
+      modal.querySelector('.modal-close')
+    );
+
+    closeBtns.forEach((btn) =>
+      btn.addEventListener('click', () => {
+        console.log('a');
+        const modal = btn.closest('.program-modal');
+
+        if (modal) {
+          closeModal(modal, blocker);
+        }
+      })
+    );
+  }
+})();
